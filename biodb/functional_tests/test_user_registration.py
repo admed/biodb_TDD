67c6df19175d73abb0ba900da75e2560645a738e
Mateusz <mateusz.kirmuc@gmail.com>
2017-06-28 13:35:34 +0200

    Create initial scenarioof user registration

---
+++ biodb/functional_tests/test_user_registration.py
@@ -0,0 +1,18 @@
+# User heard about biodb app and decide to visit this website. Received adress
+# leads him to welcome page. Welcome page contains welcome message, login form
+# with two fields and link to sign up form.
+
+# Excited user clicks sign up link. He noticed new form with several fields:
+# username, email adress, password and repeat password.
+
+# Distracted user accidentaly clicks submit button before fill any field. Every
+# field is requried so now he sees error messages bound with any field.
+
+# Immediately decides to repair his mistake and starts to fill the form with
+# data. But he dont know that user with such username, email and password
+# already exists in database. He quickly learns about it when hr clicks submit
+# button. Above every field now appear message with appropriate message:
+# 'user with such <field> already exists'.
+
+# User fills form with new data and clicks submit button once again. Success! He
+# is redirected to a welcome page.
