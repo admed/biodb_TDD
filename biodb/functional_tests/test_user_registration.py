d4dc42d185882c8a184f7e25a08b313863e8a00a
Mateusz <mateusz.kirmuc@gmail.com>
2017-06-29 15:16:43 +0200

    after click in sign-up form, user is redirect to /accounts/sign-up/

--- biodb/functional_tests/test_user_registration.py
+++ biodb/functional_tests/test_user_registration.py
@@ -38,14 +38,30 @@ class UserRegistrationTests(LiveServerTestCase):
         link = self.browser.find_element_by_id("sign_up_link")
         self.assertEqual(link.text, "Sign up")
 
-        self.fail("Finish test!")
 
-        # Excited user clicks sign up link. He noticed new form with several
-        # fields: username, email adress, password and repeat password.
+        # Excited user clicks sign up link. Current relative url is:
+        # '/accounts/sing-up/'.
+        link.click()
+        self.assertEqual(
+            self.browser.current_url,
+            self.live_server_url + "/accounts/sign-up/"
+        )
+
+        # User noticed new form with several fields: username, email adress,
+        # password and repeat password. Each has its own placeholder
+
+        # sign_up_form = self.browser.find_element_by_id("sign_up_form")
+        # username_input = sign_up_form.find_element_by_id("username_input")
+        # email_input = sign_up_form.find_element_by_id("email_input")
+        # password_input = sign_up_form.find_element_by_id("password_input")
+        # repeat_input = sign_up_form.find_element_by_id("repeat_input")
+
+
 
         # Distracted user accidentaly clicks submit button before fill any
         # field. Every field is requried so now he sees error messages bound
         # with any field.
+        self.fail("Finish test!")
 
         # Immediately decides to repair his mistake and starts to fill the form
         # with data. But he dont know that user with such username, email and
