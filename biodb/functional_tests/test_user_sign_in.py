054edc6549c793413bcb764b9343a6cba80fa7f0
Mateusz <mateusz.kirmuc@gmail.com>
2017-07-11 16:26:57 +0200

    Refactoring: FT

--- biodb/functional_tests/test_user_sign_in.py
+++ biodb/functional_tests/test_user_sign_in.py
@@ -2,6 +2,14 @@ from functional_tests.base import FunctionalTest
 from django.contrib.auth.models import User
 import time
 class UserSignInTests(FunctionalTest):
+    def __init__(self, *args, **kwargs):
+        super(UserSignInTests, self).__init__(*args, **kwargs)
+        self.login_input = lambda: self.browser.find_element_by_id(
+                                                                  "login_input")
+        self.password_input = lambda: self.browser.find_element_by_id(
+                                                               "password_input")
+        self.submit_button = lambda: self.browser.find_element_by_id(
+                                                                "submit_button")
     def test_user_encounters_login_page(self):
         # User heard about biodb app and decide to visit this website. Received
         # adress leads him to welcome page. Welcome page contains welcome
@@ -22,13 +30,13 @@ class UserSignInTests(FunctionalTest):
         description.text,
         "App that helps you manage your biological research"
         )
-        login_form = self.browser.find_element_by_id("login_form")
-        form_inputs = login_form.find_elements_by_tag_name("input")
-        submit_button = self.browser.find_element_by_id("submit_button")
-        form_inputs_id = [input_.get_attribute("id") for input_ in form_inputs]
+        self.assertEqual(
+            self.login_input().get_attribute("id"), "login_input"
+        )
+        self.assertEqual(
+            self.password_input().get_attribute("id"), "password_input"
+        )
 
-        for ID in ["login_input", "password_input"]:
-            self.assertIn(ID, form_inputs_id)
         self.assertEqual(submit_button.text, "Sign up")
 
         link = self.browser.find_element_by_id("sign_up_link")
@@ -43,13 +51,10 @@ class UserSignInTests(FunctionalTest):
         # username and password and hit submit button. After succesful login
         # he get access to first BioDB page: "/projects/".
         self.browser.get(self.live_server_url)
-        login_input = self.browser.find_element_by_id("login_input")
-        password_input = self.browser.find_element_by_id("password_input")
-        submit_button = self.browser.find_element_by_id("submit_button")
 
-        login_input.send_keys("VitoCorleone")
-        password_input.send_keys("cosa_nostra")
-        submit_button.click()
+        self.login_input().send_keys("VitoCorleone")
+        self.password_input().send_keys("cosa_nostra")
+        self.submit_button().click()
         self.assertEqual(
                   self.browser.current_url, self.live_server_url + "/projects/")
 
@@ -79,13 +84,10 @@ class UserSignInTests(FunctionalTest):
         user = User.objects.create_user(
                                username="VitoCorleone", password="cosa_nostra")
         self.browser.get(self.live_server_url)
-        login_input = self.browser.find_element_by_id("login_input")
-        password_input = self.browser.find_element_by_id("password_input")
-        submit_button = self.browser.find_element_by_id("submit_button")
 
-        login_input.send_keys("VitoCorleone")
-        password_input.send_keys("vendetta")
-        submit_button.click()
+        self.login_input().send_keys("VitoCorleone")
+        self.password_input().send_keys("vendetta")
+        self.submit_button().click()
 
         error_div = self.browser.find_element_by_class_name("nonfield")
 
