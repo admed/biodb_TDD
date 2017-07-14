40f324f631af40583e3e799430ae5553bb92b19b
Mateusz <mateusz.kirmuc@gmail.com>
2017-07-14 12:25:50 +0200

    Apply project name validation

--- biodb/projects/models.py
+++ biodb/projects/models.py
@@ -1,5 +1,13 @@
 from django.db import models
-
+from django.core.exceptions import ValidationError
 # Create your models here.
 class Project(models.Model):
     name = models.CharField(max_length=100)
+
+    def clean(self):
+        allowed_name_chars="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxy\
+                                                                   z0123456789_"
+        msg = "Name must be composed from letters, numbers or underscores."
+        for char in self.name:
+            if char not in allowed:
+                raise ValidationError({"name":msg})
--- biodb/projects/tests/test_models.py
+++ biodb/projects/tests/test_models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 from projects.models import Project
 from django.db import models
@@ -6,3 +7,11 @@ class ProjectModelTestCase(TestCase):
     def test_fields_classes(self):
         name_field = Project._meta.get_field("name")
         self.assertIsInstance(name_field, models.CharField)
+
+    def test_proper_name_validation(self):
+        p = Project.objects.create(name="Muhammad Ali")
+        msg = "Name must be composed from letters, numbers or underscores."
+        with self.assertRaises(ValidationError) as ex:
+            p.full_clean()
+        self.assertIn("name", ex.exception.message_dict)
+        self.assertIn(msg, ex.exception.message_dict["name"])
