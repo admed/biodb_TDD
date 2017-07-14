e54ea02fa6bad85f879354303f54540ab18a8a90
Mateusz <mateusz.kirmuc@gmail.com>
2017-07-14 10:43:41 +0200

    Refactoring: ProjectListView inherits now from ListView

--- biodb/projects/views.py
+++ biodb/projects/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic.list import ListView
 from projects.models import Project
 # Create your views here.
-class ProjectListView(View):
-    def get(self, request, **kwargs):
-        projects = Project.objects.all()
-        return render(request, "projects/project_list.html", {"project_list":projects})
+class ProjectListView(ListView):
+    model = Project
+    # def get(self, request, **kwargs):
+    #     projects = Project.objects.all()
+    #     return render(request, "projects/project_list.html", {"project_list":projects})
