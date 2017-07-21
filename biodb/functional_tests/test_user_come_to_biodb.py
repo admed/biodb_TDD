c250a481cc1f742ac52e7678ef2d97e7ce5548a5
Mateusz <mateusz.kirmuc@gmail.com>
2017-07-21 09:43:20 +0200

    Adjust FT after change button text in template

--- biodb/functional_tests/test_user_come_to_biodb.py
+++ biodb/functional_tests/test_user_come_to_biodb.py
@@ -1,4 +1,6 @@
 from functional_tests.base import FunctionalTest
+import time
+from django.contrib.auth.models import User
 class UserComeToBiodb(FunctionalTest):
     def test_anonymous_user_come_to_biodb(self):
         # Annonymous user come to biodb. He is redirected to login page.
@@ -8,6 +10,8 @@ class UserComeToBiodb(FunctionalTest):
 
     def test_logged_user_come_to_biodb(self):
         # Logged user come to biodb. He is redirected to projects page.
+        User.objects.create_user(username="USERNAME", password="PASSWORD")
         self.browser.get(self.live_server_url)
+        self.login_user(username="USERNAME", password="PASSWORD")
         expected_current_url = self.live_server_url + "/projects/"
         self.assertEqual(expected_current_url, self.browser.current_url)
