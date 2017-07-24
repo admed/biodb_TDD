4df863ee883b8f6f1381dbbdc50b569a345fcbf7
Mateusz <mateusz.kirmuc@gmail.com>
2017-07-24 13:34:14 +0200

    Add style FT to robjects page

--- biodb/functional_tests/test_static_files.py
+++ biodb/functional_tests/test_static_files.py
@@ -1,5 +1,7 @@
 from functional_tests.base import FunctionalTest
-
+from django.contrib.auth.models import User
+from projects.models import  Project, Robject
+import time
 
 class StaticFilesTests(FunctionalTest):
     def test_login_page(self):
@@ -9,7 +11,30 @@ class StaticFilesTests(FunctionalTest):
         # He noticed that login form is nicely centered.
         login_button = self.browser.find_element_by_id("submit_button")
         self.assertAlmostEqual(
-            login_button.location["x"] + login_button.size["width"]/2,
+            login_button.location["x"] + login_button.size["width"] / 2,
             512,
             delta=10
         )
+
+    def test_robjects_list(self):
+        ## create user and log him in
+        u = User.objects.create_user(username="USERNAME", password="PASSWORD")
+        self.login_user(username="USERNAME", password="PASSWORD")
+
+        ## create project
+        p = Project.objects.create(name="project_1")
+
+        ## create robject inside project
+        Robject.objects.create(project=p, author=u)
+
+        # User goes to robjects page.
+        self.browser.get(self.live_server_url + "/projects/project_1/robjects/")
+        self.browser.set_window_size(1024, 768)
+
+        # He noticed that robjects table is nicely centered.
+        robjects_table = self.browser.find_element_by_css_selector(
+            "#robjects_table")
+        self.assertAlmostEqual(
+            robjects_table.location["x"] + robjects_table.size["width"] / 2,
+            512,
+            delta=10)
