511dda26b52d229971cc99af8321aea86bfdbbd8
Mateusz <mateusz.kirmuc@gmail.com>
2017-09-26 09:25:27 +0200

    Fix bug after merge

--- biodb/biodb/views.py
+++ biodb/biodb/views.py
@@ -2,9 +2,10 @@ from django.views.generic import View
 from django.shortcuts import redirect
 from django.core.urlresolvers import reverse
 
+
 class RedirectView(View):
     def get(self, request, **kwargs):
         if request.user.is_authenticated():
-            return redirect(reverse("projects_list"))
+            return redirect(reverse("projects:projects_list"))
         else:
             return redirect(reverse("login"))
