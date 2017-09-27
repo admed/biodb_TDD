522c04985e0fcb94fce7a37a9fd5f81b103d80fd
Mateusz <mateusz.kirmuc@gmail.com>
2017-09-27 12:37:13 +0200

    Copy FT from spike

--- biodb/functional_tests/test_robject_delete.py
+++ biodb/functional_tests/test_robject_delete.py
@@ -2,6 +2,9 @@ from functional_tests.base import FunctionalTest
 from projects.models import Project
 from robjects.models import Robject
 from django.contrib.auth.models import User
+import time
+from guardian.shortcuts import assign_perm
+from selenium.common.exceptions import NoSuchElementException
 
 
 class RobjectDeleteTestCase(FunctionalTest):
@@ -27,3 +30,178 @@ class RobjectDeleteTestCase(FunctionalTest):
         for row in rows:
             row.find_element_by_css_selector(
                 "td:first-child input[type='checkbox']")
+
+    def test_annonymous_user_goes_to_confirmation_page(self):
+        # SET UP
+        proj = Project.objects.create(name="sample_proj")
+        robj = Robject.objects.create(project=proj, name="sample_robj")
+
+        # Annonymous user goes to robject delete confirmation page
+        self.browser.get(self.live_server_url +
+                         f"/projects/{proj.name}/robjects/delete/?{robj.name}={robj.id}")
+        # He is redirect to login page.
+        self.assertEqual(self.browser.current_url,
+                         self.live_server_url +
+                         f"/accounts/login/?next=/projects/{proj.name}/robjects/")
+
+    def test_user_without_delete_permission_goes_to_confirmation_page(self):
+        # SET UP
+        proj, user = self.set_up_robject_list()
+        robj = Robject.objects.create(project=proj, name="sample_robj")
+
+        # User goes to 'sample_robj' delete confirmation page.
+        self.browser.get(self.live_server_url +
+                         f"/projects/{proj.name}/robjects/delete/?{robj.name}={robj.id}")
+
+        # He gets permission denied message.
+        error = self.browser.find_element_by_css_selector("h1")
+        self.assertEqual(error.text, "403 Forbidden")
+
+    def test_user_can_delete_single_robject(self):
+        # SET UP
+        proj, user = self.set_up_robject_list()
+        assign_perm("can_delete_robjects", user, proj)
+        robj = Robject.objects.create(project=proj, name="sample_robj")
+
+        # User goes to robject list page.
+        self.browser.get(self.live_server_url +
+                         f"/projects/{proj.name}/robjects/")
+        # He marks one robject in table and clicks delete button.
+        self.browser.find_element_by_css_selector(
+            f".row.{robj.name} .checkbox").click()
+        self.browser.find_element_by_css_selector(f".delete-button").click()
+
+        # User sees page with confirmation message, confirm button and
+        # 'get back' link.
+        confirmation_message = self.browser.find_element_by_css_selector(
+            ".confirmation-message")
+        self.assertEqual(
+            confirmation_message.text,
+            f"Are you sure you want to delete following robject(s): {robj.name} ?"
+        )
+        confirm_button = self.browser.find_element_by_css_selector(
+            ".confirm-delete")
+        self.assertEqual(confirm_button.get_attribute("value"), "Confirm")
+        link = self.browser.find_element_by_css_selector(".get-back-link")
+        self.assertEqual(link.text, "Back to robjects list.")
+
+        # He clicks confirm button and gets back to robject list page.
+        confirm_button.click()
+        self.assertEqual(self.browser.current_url,
+                         self.live_server_url + f"/projects/{proj.name}/robjects/")
+
+        # He doesn't see now robject row in table.
+        with self.assertRaises(NoSuchElementException):
+            self.browser.find_element_by_css_selector(f".row .{robj.name}")
+
+    def test_user_try_to_delete_multiple_objects(self):
+        # SET UP
+        proj, user = self.set_up_robject_list()
+        assign_perm("can_delete_robjects", user, proj)
+        robj_1 = Robject.objects.create(project=proj, name="sample_robj_1")
+        robj_2 = Robject.objects.create(project=proj, name="sample_robj_2")
+
+        # User goes to robject page.
+        self.browser.get(self.live_server_url +
+                         f"/projects/{proj.name}/robjects/")
+        # He sees two robjects in table and marks them, then he clicks delete
+        # button.
+        rows = self.browser.find_elements_by_css_selector(".row")
+        self.assertEqual(len(rows), 2)
+        for row in rows:
+            row.find_element_by_css_selector(".checkbox").click()
+        self.browser.find_element_by_css_selector(".delete-button").click()
+
+        # User see appriopriate message and clicks confirmation button.
+        confirmation_message = self.browser.find_element_by_css_selector(
+            ".confirmation-message")
+        self.assertEqual(
+            confirmation_message.text,
+            f"Are you sure you want to delete following robject(s): {robj_1.name}, {robj_2.name} ?"
+        )
+        self.browser.find_element_by_css_selector(".confirm-delete").click()
+
+        # He is redirected to robject list page and he sees no robjects in table.
+        self.assertEqual(self.browser.current_url,
+                         self.live_server_url + f"/projects/{proj.name}/robjects/")
+        rows = self.browser.find_elements_by_css_selector(".row")
+        self.assertEqual(len(rows), 0)
+
+    def test_user_clicks_delete_button_without_select_any_robjects(self):
+        # SET UP
+        proj, user = self.set_up_robject_list()
+        assign_perm("can_delete_robjects", user, proj)
+        robj_1 = Robject.objects.create(project=proj, name="robj_1")
+        robj_2 = Robject.objects.create(project=proj, name="robj_2")
+
+        # User goes to robjects page.
+        self.browser.get(self.live_server_url +
+                         f"/projects/{proj.name}/robjects/")
+
+        # User confirms that any robject row isnt selected.
+        self.assertEqual(
+            len(self.browser.find_elements_by_css_selector(".checkbox:checked")), 0)
+
+        # He clicks delete button but nothing happens. User stays in the same
+        # page and number of rows in the table is the same.
+        current_url_before_click = self.browser.current_url
+        self.browser.find_element_by_css_selector(".delete-button").click()
+        self.assertEqual(self.browser.current_url, current_url_before_click)
+        rows = self.browser.find_elements_by_css_selector(".row")
+        self.assertEqual(len(rows), 2)
+
+    def test_user_select_all_rows_when_none_is_selected(self):
+        # SET UP
+        proj, user = self.set_up_robject_list()
+        assign_perm("can_delete_robjects", user, proj)
+        robj_1 = Robject.objects.create(project=proj, name="robj_1")
+        robj_2 = Robject.objects.create(project=proj, name="robj_2")
+        robj_3 = Robject.objects.create(project=proj, name="robj_3")
+
+        # User goes to robjects page.
+        self.browser.get(self.live_server_url +
+                         f"/projects/{proj.name}/robjects/")
+
+        # User confirms that any robject row isnt selected.
+        self.assertEqual(
+            len(self.browser.find_elements_by_css_selector(".checkbox:checked")), 0)
+
+        # He clicks select-all checkbox and sees that every checkbox is selected.
+        self.browser.find_element_by_css_selector(".select-all").click()
+        self.assertEqual(
+            len(self.browser.find_elements_by_css_selector(".checkbox:checked")), 3)
+
+        # He clicks select-all checkbox again and sees that none checkbox is
+        # selected.
+        self.browser.find_element_by_css_selector(".select-all").click()
+        self.assertEqual(
+            len(self.browser.find_elements_by_css_selector(".checkbox:checked")), 0)
+
+    def test_user_select_all_when_some_rows_are_selected(self):
+        # SET UP
+        proj, user = self.set_up_robject_list()
+        assign_perm("can_delete_robjects", user, proj)
+        robj_1 = Robject.objects.create(project=proj, name="robj_1")
+        robj_2 = Robject.objects.create(project=proj, name="robj_2")
+        robj_3 = Robject.objects.create(project=proj, name="robj_3")
+
+        # User goes to robjects page.
+        self.browser.get(self.live_server_url +
+                         f"/projects/{proj.name}/robjects/")
+
+        # He select one row.
+        self.browser.find_element_by_css_selector(".robj_1 .checkbox").click()
+        self.assertEqual(
+            len(self.browser.find_elements_by_css_selector(".checkbox:checked")), 1)
+
+        # Now, he clicks select-all checkbox and confirms that all checkboxes
+        # are selected
+        self.browser.find_element_by_css_selector(".select-all").click()
+        self.assertEqual(
+            len(self.browser.find_elements_by_css_selector(".checkbox:checked")), 3)
+
+        # He clicks select-all checkbox again and sees that none checkbox is
+        # selected.
+        self.browser.find_element_by_css_selector(".select-all").click()
+        self.assertEqual(
+            len(self.browser.find_elements_by_css_selector(".checkbox:checked")), 0)
