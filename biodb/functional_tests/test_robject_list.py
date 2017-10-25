2680983e121a48c4e45643dcd9485fd3ad4ec8a0
Mateusz <mateusz.kirmuc@gmail.com>
2017-10-25 12:57:31 +0200

    Split test_user_visit_robject_page into robject_list and robject_search

---
+++ biodb/functional_tests/test_robject_list.py
@@ -0,0 +1,63 @@
+from functional_tests.base import FunctionalTest
+from django.test import tag
+from robjects.models import Robject
+
+
+class UserVisitRobjectsPage(FunctionalTest):
+    @tag('slow')
+    def test_annonymous_user_visit_robjects_page(self):
+        self.annonymous_testing_helper(self.ROBJECT_LIST_URL)
+
+    @tag('slow')
+    def test_logged_user_visit_robjects_page___no_robjects_exists(self):
+        proj, usr = self.default_set_up_for_robjects_pages()
+
+        # Logged user visit robjects page. He sees robjects table. Table has
+        # several columns: robject id, robject name, robject create date,
+        # robject author.
+        self.browser.get(
+            self.live_server_url + f"/projects/{proj.name}/robjects")
+        header_row = self.browser.find_element_by_id("header_row")
+        table_columns = header_row.find_elements_by_tag_name("th")
+        table_columns_names = [column.text for column in table_columns]
+
+        self.assertIn("id", table_columns_names)
+        self.assertIn("name", table_columns_names)
+        self.assertIn("author", table_columns_names)
+        self.assertIn("create by", table_columns_names)
+        self.assertIn("create date", table_columns_names)
+        self.assertIn("modify by", table_columns_names)
+
+        # Table hasnt any rows
+        robject_rows = self.browser.find_elements_by_css_selector(
+            ".row.robject")
+        self.assertEqual(len(robject_rows), 0)
+
+    @tag('slow')
+    def test_logged_user_visit_robjects_page___robjects_exists_in_project(self):
+        proj, usr = self.default_set_up_for_robjects_pages()
+
+        # Create sample robjects.
+        robj1 = Robject.objects.create(
+            author=usr, project=proj, name="robject_1")
+        robj2 = Robject.objects.create(
+            author=usr, project=proj, name="robject_2")
+        robj3 = Robject.objects.create(
+            author=usr, project=proj, name="robject_3")
+
+        # Logged user goes to project_1's robjects page. He knows that this
+        # project contains several robjects. He sees table of rows. Each row
+        # contains data of singe robject from this project.
+
+        self.browser.get(self.live_server_url + "/projects/project_1/robjects")
+        # rows has class names : "row robject_<robject pk>"
+        # capture all rows
+        robject_rows = self.browser.find_elements_by_css_selector(
+            ".row")
+        self.assertEqual(len(robject_rows), 3)
+
+        for robject in [robj1, robj2, robj3]:
+            # capture specific row
+            row = self.browser.find_element_by_class_name(robject.name)
+            self.assertIn(str(robject.id), row.text)
+            self.assertIn(str(robject.author), row.text)
