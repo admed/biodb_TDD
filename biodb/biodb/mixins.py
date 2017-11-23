53ec46d085e23c3439ca185a90a34225a83269b9
Adrian <adrian.jasinski@adamed.com.pl>
2017-11-23 12:25:36 +0100

    move get_permission_object method from views to mixin

--- biodb/biodb/mixins.py
+++ biodb/biodb/mixins.py
@@ -1,6 +1,9 @@
+from django.http import Http404
 from django.http import HttpResponseForbidden
 from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
 from biodb import settings
+from projects.models import Project
 
 
 class LoginRequiredMixin(object):
@@ -22,3 +25,14 @@ class LoginPermissionRequiredMixin(object):
                         f"<h1>User doesn't have permission: {_permission}</h1>")
             return super().dispatch(request, *args, **kwargs)
         return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+
+    def get_permission_object(self):
+        """methtod returning permission object for guardian.
+
+        project_name is required in kwargs else method is returning 404.
+        """
+        if self.kwargs and 'project_name' in self.kwargs:
+            project = get_object_or_404(
+                Project, name=self.kwargs['project_name'])
+            return project
+        return Http404
